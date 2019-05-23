0298c4cf5a209ee864ba75c02133f722e40d6bf5
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-23 15:20:50 -0600

    stuff seeded

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,45 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+  end
+
+  private
+  def set_board
+    @board = Board.find(params[:id])
+  end
+  def board_params
+    params.permit(:board).require(:board_name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do 
+  board = Board.create(
+    board_name: Faker::Book.genre,
+    )
+    3.times do
+      list = List.create(
+        list_name: Faker::Book.title,
+        board_id: board.id
+
+      )
+      3.times do
+        Task.create(
+          task_name: Faker::TvShows::RickAndMorty.location,
+          description: Faker::TvShows::RickAndMorty.quote,
+          list_id: list.id
+        )end
+        end
+      end
+
+      puts "We created 3 boards 9 lists and 27 task"
